4ecec74f80dee4c3648a8480687a68c22ae150e6
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-10-10 10:06:30 +0500

    viewbinding funcational

--- app/src/main/java/com/muhsantech/calculator_app/MainActivity.kt
+++ app/src/main/java/com/muhsantech/calculator_app/MainActivity.kt
@@ -2,10 +2,17 @@ package com.muhsantech.calculator_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.muhsantech.calculator_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        
     }
 }
