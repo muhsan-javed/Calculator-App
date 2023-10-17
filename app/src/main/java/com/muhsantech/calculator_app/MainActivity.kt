dab12d392628ab43e9e35edc2f98a2a578f534ac
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-10-17 08:50:41 +0500

    Clear Code

--- app/src/main/java/com/muhsantech/calculator_app/MainActivity.kt
+++ app/src/main/java/com/muhsantech/calculator_app/MainActivity.kt
@@ -6,7 +6,7 @@ import com.muhsantech.calculator_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
